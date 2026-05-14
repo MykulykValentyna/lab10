5f416f2f58a979591f71d82ef11d171c984f7f5f
Valentyna <mikulik.valentina@gmail.com>
2026-05-14 11:33:33 +0300

    Додано перехід на сторінку бронювання

--- RailwayStation/src/components/TrainCard.jsx
+++ RailwayStation/src/components/TrainCard.jsx
@@ -1,9 +1,15 @@
-import styles from './TrainCard.module.css'
+import { useNavigate } from 'react-router-dom';
+import styles from './TrainCard.module.css';
 
 export const TrainCard = ({ train }) => {
+  const navigate = useNavigate();
   const departureDate = new Date(train.departureTime);
   const formattedDate = departureDate.toLocaleDateString('uk-UA');
   const formattedTime = departureDate.toLocaleTimeString('uk-UA', { hour: '2-digit', minute: '2-digit' });
+
+  const handleBookingClick = () => {
+    navigate(`/booking/${train.id}`);
+  };
 
   return (
     <div className={styles.card}>
